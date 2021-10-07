d0fd1f295c6b7b6d0308420f1d05c310c768e256
AgostonSz <szabo.agoston1997@gmail.com>
2021-10-07 13:09:20 +0200

    removed unused stuff

--- letterrecognition.py
+++ letterrecognition.py
@@ -34,11 +34,6 @@ NN1 = init(5*5, 3)
 def sigmoid(x):
     return 1 / (np.exp(-x) + 1)
 
-#derivative of sigmoid
-def d_sigmoid(x):
-    return (np.exp(-x)) / ((np.exp(-x) +1 ) ** 2)
-
-
 #Softmax
 def softmax(x):
     exp_element = np.exp(x - x.max())
